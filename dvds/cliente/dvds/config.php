0ab63b802ab44b8c0c75bae34166a0b3fa0ddea0
Luis Santocildes <luis.santocildes@gmail.com>
2016-02-10 22:59:28 +0100

    Folder dvd duplicated

--- dvds/cliente/dvds/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$baseFolder = '/opt/dvds/';
-$mediaFolder = '/opt/dvds/media/';
-$omxplayer = '/usr/bin/omxplayer';
-$downloadFolder = '/opt/dvds/download/';
-
-?>
--- dvds/servidor/html/prueba.php
+++
@@ -1,5 +0,0 @@
-<?php
-$clave = ini_get("session.upload_progress.prefix") . $_POST[ini_get("session.upload_progress.name")];
-echo "$clave<br>";
-var_dump($_SESSION[$clave]);
-?>
